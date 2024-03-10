f64b47b0598e2a74a4eb0e035e0e2cd76f4db387
sagarpatil30 <sagarpatil30720@gamil.com>
2024-03-11 00:08:56 +0530

    addded ECMA-standards

---
+++ ECMA-standards/index.js
@@ -0,0 +1,5 @@
+"use strict"  //new version of JS
+
+alert('hello')
+
+window.alert('hello')
--- let-var-const/index.js
+++ let-var-const/index.js
@@ -1,1 +1,23 @@
-console.log('javascript is fun');
+// CONST
+const accountName ='Developer';
+// const cannot repeat the vaule 
+// accountName ='Tester'
+console.log('Name '+ accountName);
+
+
+// LET
+// let always accepts new value
+let accountId = 1010;
+accountId = 333
+accountId = 444
+console.log(accountId);
+
+// VAR
+// VAR always accepts new value
+var accountNo = 5050; 
+accountNo= 5111;
+console.log(accountNo);
+
+
+// logging the table
+console.table([accountName,accountId,accountNo]);
