b7d2f4710fa480ae98793847de96778b326d3742
sagarpatil30 <sagarpatil30720@gamil.com>
2024-03-11 00:25:44 +0530

    type of , null and undefined

--- ECMA-standards/index.js
+++ ECMA-standards/index.js
@@ -1,5 +1,20 @@
-"use strict"  //new version of JS
+// "use strict"  //new version of JS
 
-alert('hello')
+// alert('hello')
+// window.alert('hello')
 
-window.alert('hello')
+
+// undefined
+// the value has been declared but not used yet
+let a;
+
+// null
+// the value can be assignable but we can set it as null
+let b= null
+
+
+// type of NUll is object
+console.log(typeof null);
+
+// type of undefined is undefined
+console.log(typeof undefined);
