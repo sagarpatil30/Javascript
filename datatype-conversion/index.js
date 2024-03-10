20aaa9d5e9dff0857bf175f7c774196d00801e81
sagarpatil30 <sagarpatil30720@gamil.com>
2024-03-11 00:46:57 +0530

    NAN, datatype conversions

---
+++ datatype-conversion/index.js
@@ -0,0 +1,19 @@
+// not a number
+// NAN
+let value = "50kjhgf";
+let theValue =  Number(value)
+console.log( theValue);
+console.log(typeof theValue);
+
+
+let b = "50"
+let bNumber = Number(b)
+console.log( typeof bNumber);
+
+let c = null
+let cNumber = Number(c)
+console.log(cNumber);
+
+let d = true
+let dNumber = Number(d)
+console.log(dNumber);
