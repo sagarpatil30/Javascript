e87b9b274316c401accde96932dd4763600dbc0d
sagar30 <sagarpatil30720@gamil.com>
2024-03-11 22:45:30 +0530

    string to Number

---
+++ stringTonumber/index.js
@@ -0,0 +1,9 @@
+console.log("1"+3+1);
+
+console.log(1+"4"+1);
+
+// When a number is followed by a string with the + operator, JavaScript converts the number to a string and performs string concatenation.
+console.log(1+2+"1");
+
+
+console.log(+true);
