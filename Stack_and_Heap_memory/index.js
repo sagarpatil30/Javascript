c1bc9045087d010da1ed7611dfc437a815fddf4a
sagar30 <sagarpatil30720@gmail.com>
2024-03-16 13:07:24 +0530

    heap stack in js

--- Stack_and_Heap_memory/index.js
+++ Stack_and_Heap_memory/index.js
@@ -8,5 +8,26 @@
 // Size is known at run time
 // No limit for object memory
 
-let a = 10+20;
-console.log(a);
+
+
+// example for stack memory allocation
+// it always give the copy and never changes the original value
+let newname ='js';
+let anotherName = newname;
+anotherName ='react'
+console.log(anotherName);
+console.log(newname);
+
+
+// heap memory allocation
+// it always gets the reference 
+let user={
+email:"test@123",
+testno:"123"
+}
+;
+
+let user1 = user
+user1.email ='test@321'
+console.log(user);
+console.log(user1);
