f96d436d383fa6e962309635371f0fa32732d301
sagar30 <sagarpatil30720@gmail.com>
2024-03-16 14:14:51 +0530

    added strings

---
+++ strings.js
@@ -0,0 +1,20 @@
+const course ='javascript';
+const courseFee= 1200;
+
+// not a good pratice using this
+// console.log(course + courseFee);
+
+// using string interploation is new method 
+// console.log(`the coursename is ${course} and courseFee is ${courseFee}`);
+
+console.log(course.length);
+console.log(course.toUpperCase());
+console.log(course.charAt(7));
+console.log(course.indexOf('t'));
+console.log(course.__proto__);
+console.log(course.substring(0,3));
+console.log(course.slice(4));
+// let a = new String('FRONTEND-SKILLS')
+// console.log(a);
+
+console.log(course.replace);
